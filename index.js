b7cffee2e7eb45df04522bd0a87e5e1fbaccf691
Jonathan McCann <jonathan.mccann@liferay.com>
2015-08-13 09:44:45 -0700

    Add toolbar menu item as well

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+var buttons = require('sdk/ui/button/action');
 var tabs = require("sdk/tabs");
+
+var button = buttons.ActionButton({
+	id: "save-url-link",
+	label: "Save URL to Wayback Machine",
+	icon: {
+		"16": "./icon-save-16.png",
+		"32": "./icon-save-32.png",
+		"48": "./icon-save-64.png"
+	},
+	onClick: handleSaveURL
+});
 
 var contextMenu = require("sdk/context-menu");
 	var menuItem = contextMenu.Item({
@@ -8,6 +20,10 @@ var contextMenu = require("sdk/context-menu");
 					   '	self.postMessage();' +
 					   '});',
 		onMessage: function () {
-			tabs.open("http://web.archive.org/save/" + tabs.activeTab.url);
+			handleSaveURL();
 		}
 });
+
+function handleSaveURL() {
+	tabs.open("http://web.archive.org/save/" + tabs.activeTab.url);
+}
