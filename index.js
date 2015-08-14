3b9db98bd03f5c3c0fe864d2cc07465d49dcc94f
Jonathan McCann <jonathan.mccann@liferay.com>
2015-08-14 13:14:59 -0700

    Change the context menu label and function when preferences change. Also, add in some comments and source format.

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
+// Set up necessary variables via "require"
+var contextMenu = require("sdk/context-menu");
+var defaultArchiver = require('sdk/simple-prefs').prefs['defaultArchiver'];
+var preferences = require("sdk/simple-prefs").prefs;
 var { ToggleButton } = require('sdk/ui/button/toggle');
 var panels = require("sdk/panel");
 var self = require("sdk/self");
 var tabs = require("sdk/tabs");
 
+require("sdk/simple-prefs").on("defaultArchiver", onDefaultArchiverPreferenceChange);
+
+// Create string constants for the context menu label
+var archiveIsLabel = "Save to archive.is";
+var waybackMachineLabel = "Save to Wayback Machine";
+
+// Create toggle button for toolbar
 var button = ToggleButton({
 	id: "my-button",
 	label: "my button",
@@ -14,18 +25,24 @@ var button = ToggleButton({
 	onChange: handleChange
 });
 
-var contextMenu = require("sdk/context-menu");
-	var menuItem = contextMenu.Item({
-		label: "Save to Wayback Machine",
-		context: contextMenu.PageContext(),
-		contentScript: 'self.on("click", function () {' +
-					   '	self.postMessage();' +
-					   '});',
-		onMessage: function () {
+// Create context menu item
+var menuItem = contextMenu.Item({
+	label: waybackMachineLabel,
+	context: contextMenu.PageContext(),
+	contentScript: 'self.on("click", function () {' +
+				   '	self.postMessage();' +
+				   '});',
+	onMessage: function () {
+		if (defaultArchiver == "archiveIs") {
+			handleSaveArchiveIs();
+		}
+		else {
 			handleSaveWaybackMachine();
 		}
+	}
 });
 
+// Create panel to be used in the toggle button
 var panel = panels.Panel({
 	contentURL: self.data.url("panel.html"),
 	contentScriptFile: self.data.url("panel.js"),
@@ -59,6 +76,12 @@ function handleSaveArchiveIs() {
 function handleSaveWaybackMachine() {
 	panel.hide();
 	tabs.open("https://web.archive.org/save/" + tabs.activeTab.url);
+}
+
+function onDefaultArchiverPreferenceChange() {
+	defaultArchiver = preferences.defaultArchiver;
+	defaultArchiverLabel = (preferences.defaultArchiver == "archiveIs") ? archiveIsLabel : waybackMachineLabel;
+	menuItem.label = defaultArchiverLabel;
 }
 
 panel.port.on("saveArchiveIs" , function() {
